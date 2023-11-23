a461c24dfe0b9d345165126deac403ba86b15e17
Pedro Aragon <pedarf@dtu.dk>
2023-11-23 10:03:28 +0100

    Small modifications

--- GUI.py
+++ GUI.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import simpledialog, messagebox
+from tkinter import messagebox, simpledialog
 import pandas as pd
 import datetime
 import random
@@ -10,22 +10,21 @@ def generate_filename(user, system, method, condition, cell_type, position):
 
 def get_well_positions(start_row, end_row, start_col, end_col):
     rows = [chr(i) for i in range(ord(start_row), ord(end_row) + 1)]
-    columns = list(range(start_col, end_col + 1))
+    columns = [str(i) for i in range(start_col, end_col + 1)]
     return [f"{row}{col}" for row in rows for col in columns]
 
-def get_cell_types_info():
+def get_cell_types_info(plate_number):
     cell_types_info = []
-    num_cell_types = simpledialog.askinteger("Input", "Enter the number of different cell types:")
+    num_cell_types = simpledialog.askinteger("Input", f"Enter the number of different cell types for plate {plate_number}:")
     for _ in range(num_cell_types):
-        cell_type = simpledialog.askstring("Input", "Enter cell type:")
+        cell_type = simpledialog.askstring("Input", f"Enter cell type for plate {plate_number}:")
         row_range = simpledialog.askstring("Input", f"Enter row range for {cell_type} (e.g., A-F):")
-        col_range = simpledialog.askstring("Input", f"Enter column range for {cell_type} (e.g., 1-8):")
+        col_range = simpledialog.askstring("Input", f"Enter column range for {cell_type} (e.g., 1-12):")
         start_row, end_row = row_range.split('-')
         start_col, end_col = col_range.split('-')
         positions = get_well_positions(start_row.strip().upper(), end_row.strip().upper(), int(start_col.strip()), int(end_col.strip()))
         cell_types_info.append((cell_type, positions))
     return cell_types_info
-
 
 def create_csv():
     num_wells = int(num_wells_var.get())
@@ -36,30 +35,34 @@ def create_csv():
     path = path_var.get()
     inj_vol = inj_vol_var.get()
     randomize_queue = randomize_var.get()
+    same_layout = same_layout_var.get()
 
-    columns = ["Sample Type", "File Name", "Sample ID", "Path", "Instrument Method", 
-               "Process Method", "Calibration File", "Position", "Inj Vol", "Level", 
-               "Sample Wt", "Sample Vol", "ISTD Amt", "Dil Factor", "L1 Study", 
+    if same_layout:
+        cell_types_info = get_cell_types_info(1)
+        all_plates_info = [cell_types_info for _ in range(num_plates)]
+    else:
+        all_plates_info = [get_cell_types_info(i + 1) for i in range(num_plates)]
+
+    columns = ["Sample Type", "File Name", "Sample ID", "Path", "Instrument Method",
+               "Process Method", "Calibration File", "Position", "Inj Vol", "Level",
+               "Sample Wt", "Sample Vol", "ISTD Amt", "Dil Factor", "L1 Study",
                "L2 Client", "L3 Laboratory", "L4 Company", "L5 Phone", "Comment", "Sample Name"]
     df = pd.DataFrame(columns=columns)
 
-    for plate in range(num_plates):
-        condition = simpledialog.askstring("Input", f"Enter condition for plate {plate + 1}:")
-        tray = simpledialog.askstring("Input", f"Enter tray for plate {plate + 1} (B, G, R):").upper()
-        if plate == 0 or not cell_types_info:
-            cell_types_info = get_cell_types_info()
-
+    for plate, cell_types_info in enumerate(all_plates_info, start=1):
+        tray = simpledialog.askstring("Input", f"Enter tray for plate {plate} (B, G, R):").upper()
+        condition = simpledialog.askstring("Input", f"Enter condition for plate {plate}:")
         for cell_type, positions in cell_types_info:
             for position in positions:
-                tray_position = f"{tray}{position}"
                 file_name = generate_filename(user, system, method, condition, cell_type, position)
                 df = df.append({
                     "Sample Type": "Unknown",
                     "File Name": file_name,
-                    "Sample ID": 1,
+                    "Sample ID": 1,  # Update with your logic if necessary
                     "Path": path,
-                    "Position": tray_position,
+                    "Position": f"{tray}{position}",
                     "Inj Vol": inj_vol
+                    # Update other fields as necessary
                 }, ignore_index=True)
 
     if randomize_queue:
@@ -75,6 +78,7 @@ def create_csv():
 root = tk.Tk()
 root.title("Well Plate Data Generator")
 
+# Define variables
 num_wells_var = tk.StringVar()
 num_plates_var = tk.StringVar()
 user_var = tk.StringVar()
@@ -83,7 +87,9 @@ method_var = tk.StringVar()
 path_var = tk.StringVar(value="D:\\")
 inj_vol_var = tk.StringVar()
 randomize_var = tk.BooleanVar(value=False)
+same_layout_var = tk.BooleanVar(value=False)
 
+# Define interface
 tk.Label(root, text="Number of wells per plate:").pack()
 tk.Entry(root, textvariable=num_wells_var).pack()
 
@@ -104,6 +110,8 @@ tk.Entry(root, textvariable=path_var).pack()
 
 tk.Label(root, text="Injection Volume:").pack()
 tk.Entry(root, textvariable=inj_vol_var).pack()
+
+tk.Checkbutton(root, text="Same sample position layout for all plates", variable=same_layout_var).pack()
 tk.Checkbutton(root, text="Randomize Queue", variable=randomize_var).pack()
 
 generate_button = tk.Button(root, text="Generate CSV", command=create_csv)
