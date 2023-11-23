abdd11a3dbf20cfca598da0861a271cdb001032c
Pedro Aragon <pedarf@dtu.dk>
2023-11-23 13:46:01 +0100

    Added logo

--- GUI.py
+++ GUI.py
@@ -1,18 +1,21 @@
 import tkinter as tk
-from tkinter import messagebox, simpledialog
+from tkinter import messagebox, simpledialog, PhotoImage
 import pandas as pd
 import datetime
 import random
 
+# Function to generate a filename based on given parameters
 def generate_filename(user, system, method, condition, cell_type, position):
     current_date = datetime.date.today().strftime("%Y%m%d")
     return f"{current_date}_{user}_{system}_{method}_{condition}_{cell_type}_{position}"
 
+# Function to get well positions based on the start and end rows and columns
 def get_well_positions(start_row, end_row, start_col, end_col):
     rows = [chr(i) for i in range(ord(start_row), ord(end_row) + 1)]
     columns = [str(i) for i in range(start_col, end_col + 1)]
     return [f"{row}{col}" for row in rows for col in columns]
 
+# Function to gather information about cell types for a plate
 def get_cell_types_info(plate_number):
     cell_types_info = []
     num_cell_types = simpledialog.askinteger("Input", f"Enter the number of different cell types for plate {plate_number}:")
@@ -26,6 +29,7 @@ def get_cell_types_info(plate_number):
         cell_types_info.append((cell_type, positions))
     return cell_types_info
 
+# Function to create a CSV file based on the input data
 def create_csv():
     num_wells = int(num_wells_var.get())
     num_plates = int(num_plates_var.get())
@@ -75,46 +79,60 @@ def create_csv():
 
     messagebox.showinfo("Success", f"Data exported to {output_file_name}")
 
-root = tk.Tk()
-root.title("Well Plate Data Generator")
+# Function to create the GUI
+def create_gui():
+    root = tk.Tk()
+    root.title("Well Plate Data Generator")
 
-# Define variables
-num_wells_var = tk.StringVar()
-num_plates_var = tk.StringVar()
-user_var = tk.StringVar()
-system_var = tk.StringVar()
-method_var = tk.StringVar()
-path_var = tk.StringVar(value="D:\\")
-inj_vol_var = tk.StringVar()
-randomize_var = tk.BooleanVar(value=False)
-same_layout_var = tk.BooleanVar(value=False)
+    # Load the logo image
+    logo_path = '/Users/Pedarf/Desktop/Python scripts/qGenerator/250px-Arwing_Starlink.png'  # Replace with your local path
+    logo_image = PhotoImage(file=logo_path)
 
-# Define interface
-tk.Label(root, text="Number of wells per plate:").pack()
-tk.Entry(root, textvariable=num_wells_var).pack()
+    # Display the logo image
+    logo_label = tk.Label(root, image=logo_image)
+    logo_label.pack()
 
-tk.Label(root, text="Number of plates:").pack()
-tk.Entry(root, textvariable=num_plates_var).pack()
+    # Define variables
+    global num_wells_var, num_plates_var, user_var, system_var, method_var, path_var, inj_vol_var, randomize_var, same_layout_var
+    num_wells_var = tk.StringVar()
+    num_plates_var = tk.StringVar()
+    user_var = tk.StringVar()
+    system_var = tk.StringVar()
+    method_var = tk.StringVar()
+    path_var = tk.StringVar(value="D:\\")
+    inj_vol_var = tk.StringVar()
+    randomize_var = tk.BooleanVar(value=False)
+    same_layout_var = tk.BooleanVar(value=False)
 
-tk.Label(root, text="User Identifier:").pack()
-tk.Entry(root, textvariable=user_var).pack()
+    # Define interface
+    tk.Label(root, text="Number of wells per plate:").pack()
+    tk.Entry(root, textvariable=num_wells_var).pack()
 
-tk.Label(root, text="System Used:").pack()
-tk.Entry(root, textvariable=system_var).pack()
+    tk.Label(root, text="Number of plates:").pack()
+    tk.Entry(root, textvariable=num_plates_var).pack()
 
-tk.Label(root, text="Method Applied:").pack()
-tk.Entry(root, textvariable=method_var).pack()
+    tk.Label(root, text="User Identifier:").pack()
+    tk.Entry(root, textvariable=user_var).pack()
 
-tk.Label(root, text="Path:").pack()
-tk.Entry(root, textvariable=path_var).pack()
+    tk.Label(root, text="System Used:").pack()
+    tk.Entry(root, textvariable=system_var).pack()
 
-tk.Label(root, text="Injection Volume:").pack()
-tk.Entry(root, textvariable=inj_vol_var).pack()
+    tk.Label(root, text="Method Applied:").pack()
+    tk.Entry(root, textvariable=method_var).pack()
 
-tk.Checkbutton(root, text="Same sample position layout for all plates", variable=same_layout_var).pack()
-tk.Checkbutton(root, text="Randomize Queue", variable=randomize_var).pack()
+    tk.Label(root, text="Path:").pack()
+    tk.Entry(root, textvariable=path_var).pack()
 
-generate_button = tk.Button(root, text="Generate CSV", command=create_csv)
-generate_button.pack()
+    tk.Label(root, text="Injection Volume:").pack()
+    tk.Entry(root, textvariable=inj_vol_var).pack()
 
-root.mainloop()
+    tk.Checkbutton(root, text="Same sample position layout for all plates", variable=same_layout_var).pack()
+    tk.Checkbutton(root, text="Randomize Queue", variable=randomize_var).pack()
+
+    generate_button = tk.Button(root, text="Generate CSV", command=create_csv)
+    generate_button.pack()
+
+    root.mainloop()
+
+# Call the function to create the GUI with the logo
+create_gui()
